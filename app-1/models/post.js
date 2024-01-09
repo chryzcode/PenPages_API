7e09f3d0b48e7ac531f8e1a362218dc2c8ad188d
chryzcode <alabaolanrewaju13@gmail.com>
2024-01-09 22:32:53 +0100

    push

---
+++ app-1/models/post.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+
+
+
+const tagSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Name field is compulsory"],
+    maxlength: 70,
+  },
+});
+
+const commentSchema = new mongoose.Schema(
+  {
+    body: {
+      type: String,
+      required: [true, "Body field is compulsory"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const postSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Title field is compulsory"],
+      maxlength: 70,
+    },
+    body: {
+      type: String,
+      required: [true, "Body field is compulsory"],
+      maxlength: 70,
+    },
+    comment: {
+      type: Schema.Types.ObjectId,
+      ref: "commentSchema",
+    },
+    tag: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "tagSchema",
+    },
+  },
+  { timestamps: true }
+);
