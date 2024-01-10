76f2c0b8b99d65afa43a116b0018f0081baae94c
chryzcode <alabaolanrewaju13@gmail.com>
2024-01-10 11:10:11 +0100

    push

--- app-1/app.js
+++ app-1/app.js
@@ -1,23 +1,26 @@
-require('dotenv').config()
-const express = require('express')
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const { connectDB } = require("./db/connect");
 
-app = express()
+app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('hi')
-})
+app.get("/", (req, res) => {
+  res.send("hi");
+});
 
-const port =  process.env.PORT || 3000
+const port = process.env.PORT || 3000;
+const url = process.env.MONGO_URI;
 
-const start = (async => {
-    try {
-     app.listen(port, ()=> console.log(`server is running on port ${port}`))
-    } catch (error) {
-        console.log(error)
-    }
-})
+const start = async () => {
+  try {
+    await connectDB(url);
+    app.listen(port, () => console.log(`server is running on port ${port}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-
-start()
+start();
