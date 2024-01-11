571f55867dfa7172afc94028c60c9511c45bc989
chryzcode <alabaolanrewaju13@gmail.com>
2024-01-11 23:34:43 +0100

    push

---
+++ controllers/comment.js
@@ -0,0 +1,15 @@
+const { StatusCodes } = require('http-status-codes')
+const comment = require('../models/comment')
+const Post = require('../models/post')
+
+
+const createComment = async (req, res) => {
+    const {postId} = req.params 
+    // const post = await Post.find({ _id: postId });
+    req.body.post = postId
+    req.body.user = req.user.userId
+    const comment = await Comment.create({...req.body})
+    res.status(StatusCodes.CREATED).json({comment})
+}
+
+module.exports = {createComment, }
--- models/comment.js
+++ models/comment.js
@@ -1,7 +1,12 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const commentSchema = new mongoose.Schema(
   {
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+      required: [true, "Post field is compulsory"],
+    },
     post: {
       type: mongoose.Schema.ObjectId,
       ref: "Post",
