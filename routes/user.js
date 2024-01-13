117e2071cb1f2b3ffd840177d0722589ff009ec3
chryzcode <alabaolanrewaju13@gmail.com>
2024-01-13 10:50:10 +0100

    push

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { getAllUsers, login, register } from "../controllers/user.js";
+import { getAllUsers, login, register, getUser} from "../controllers/user.js";
 
 const router = express.Router();
 
 router.route("/auth/register").post(register);
 router.route("/auth/login").post(login);
 router.route("/all-users").get(getAllUsers);
+router.route("/:username").get(getUser);
+
 
 export default router;
