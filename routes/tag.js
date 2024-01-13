cb36074bc2acc073043498f296978f19edc0e661
chryzcode <alabaolanrewaju13@gmail.com>
2024-01-13 10:25:55 +0100

    push

--- routes/tag.js
+++ routes/tag.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { createTag, getAllTags } from "../controllers/tag.js";
+import { createTag, getAllTags, getTag } from "../controllers/tag.js";
 
 const router = express.Router();
 
 router.route("/").post(createTag).get(getAllTags);
+router.route("/:tagId").get(getTag);
 
 export default router;
