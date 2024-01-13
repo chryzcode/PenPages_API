0ae695217a4989da4057acd796e62d6051c04a7f
chryzcode <alabaolanrewaju13@gmail.com>
2024-01-13 10:46:16 +0100

    push

--- controllers/user.js
+++ controllers/user.js
@@ -1,36 +1,40 @@
 import { StatusCodes } from "http-status-codes";
-import { BadRequestError, UnauthenticatedError } from "../errors/index.js";
+import { BadRequestError, UnauthenticatedError, NotFoundError } from "../errors/index.js";
 import User from "../models/user.js";
 
 export const register = async (req, res) => {
-	const user = await User.create({ ...req.body });
-	const token = user.createJWT();
-	res
-		.status(StatusCodes.CREATED)
-		.json({ user: { firstName: user.firstName, lastName: user.lastName }, token });
+  const user = await User.create({ ...req.body });
+  const token = user.createJWT();
+  res.status(StatusCodes.CREATED).json({ user: { firstName: user.firstName, lastName: user.lastName }, token });
 };
 
 export const login = async (req, res) => {
-	const { email, password } = req.body;
-	if (!email || !password) {
-		throw new BadRequestError("Put in your email and password");
-	}
-	const user = await User.findOne({ email });
-	if (!user) {
-		throw new UnauthenticatedError("User does not exist");
-	}
-	const passwordMatch = await user.comparePassword(password);
-	if (!passwordMatch) {
-		throw new UnauthenticatedError("Invalid password");
-	}
-	const token = user.createJWT();
-	res.status(StatusCodes.OK).json({ user: { firstName: user.firstName, lastName: user.lastName }, token });
+  const { email, password } = req.body;
+  if (!email || !password) {
+    throw new BadRequestError("Put in your email and password");
+  }
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new UnauthenticatedError("User does not exist");
+  }
+  const passwordMatch = await user.comparePassword(password);
+  if (!passwordMatch) {
+    throw new UnauthenticatedError("Invalid password");
+  }
+  const token = user.createJWT();
+  res.status(StatusCodes.OK).json({ user: { firstName: user.firstName, lastName: user.lastName }, token });
 };
 
 export const getAllUsers = async (req, res) => {
-	const users = await User.find({});
-	res.status(StatusCodes.OK).json({ users });
+  const users = await User.find({});
+  res.status(StatusCodes.OK).json({ users });
 };
 
-
-
+export const getUser = async (req, res) => {
+  const { username } = req.params;
+  const user = await User.findOne({ username: username });
+  if (!user) {
+    throw new NotFoundError(`User with username ${username} does not exist`);
+	}
+	res.status(StatusCodes.OK).json({user})
+};
