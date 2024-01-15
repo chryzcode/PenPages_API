1227bde145325c2dba590203754e6817b78a55eb
chryzcode <alabaolanrewaju13@gmail.com>
2024-01-15 12:04:18 +0100

    push

--- routes/post.js
+++ routes/post.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { createPost, getUserPosts, updatePost, getPost, getAllPosts } from "../controllers/post.js";
+import { createPost, getUserPosts, updatePost, getPost, getAllPosts, deletePost } from "../controllers/post.js";
 import authenticateUser from "../middleware/authentication.js";
 
 const router = express.Router();
 
 router.route("/").get(getAllPosts).post(authenticateUser, createPost);
-router.route("/:postId").get(getPost).put(authenticateUser, updatePost);
+router.route("/:postId").get(getPost).put(authenticateUser, updatePost).delete(authenticateUser, deletePost);
 
 export default router;
