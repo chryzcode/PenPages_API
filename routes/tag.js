763277169aaa5ff460370ca736807d15f3627bd9
chryzcode <alabaolanrewaju13@gmail.com>
2024-01-15 13:04:23 +0100

    push

--- routes/tag.js
+++ routes/tag.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { createTag, getAllTags, getTag, updateTag, delete } from "../controllers/tag.js";
+import { createTag, getAllTags, getTag, updateTag, deleteTag } from "../controllers/tag.js";
 
 const router = express.Router();
 
 router.route("/").post(createTag).get(getAllTags);
-router.route("/:tagId").get(getTag).put(updateTag).delete()
+router.route("/:tagId").get(getTag).put(updateTag).delete(deleteTag);
 
 export default router;
