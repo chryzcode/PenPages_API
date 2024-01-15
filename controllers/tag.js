b86820723d3042f2d9850592885034597322e560
chryzcode <alabaolanrewaju13@gmail.com>
2024-01-15 13:03:57 +0100

    push

--- controllers/tag.js
+++ controllers/tag.js
@@ -3,19 +3,19 @@ import { BadRequestError, NotFoundError } from "../errors/index.js";
 import Tag from "../models/tag.js";
 
 export const createTag = async (req, res) => {
-	const tag = await Tag.create({ ...req.body });
-	res.status(StatusCodes.CREATED).json({ tag });
+  const tag = await Tag.create({ ...req.body });
+  res.status(StatusCodes.CREATED).json({ tag });
 };
 
 export const getAllTags = async (req, res) => {
-	const tags = await Tag.find({});
-	res.status(StatusCodes.OK).json({ tags });
+  const tags = await Tag.find({});
+  res.status(StatusCodes.OK).json({ tags });
 };
 
 export const getTag = async (req, res) => {
-	const {tagId} = req.params
-	const tag = await Tag.findOne({ _id: tagId });
-	if (!tag) {
+  const { tagId } = req.params;
+  const tag = await Tag.findOne({ _id: tagId });
+  if (!tag) {
     throw new NotFoundError(`Tag with id ${tagId} does not exist`);
   }
   res.status(StatusCodes.OK).json({ tag });
@@ -23,9 +23,18 @@ export const getTag = async (req, res) => {
 
 export const updateTag = async (req, res) => {
   const { tagId } = req.params;
-	const tag = await Tag.findOneAndUpdate({ _id: tagId }, req.body, { new: true, runValidators: true });
-	if (!tag) {
-		throw new NotFoundError(`Tag with id ${tagId} does not exist`);
-	}
+  const tag = await Tag.findOneAndUpdate({ _id: tagId }, req.body, { new: true, runValidators: true });
+  if (!tag) {
+    throw new NotFoundError(`Tag with id ${tagId} does not exist`);
+  }
   res.status(StatusCodes.OK).json({ tag });
+};
+
+export const deleteTag = async (req, res) => {
+  const { tagId } = req.params;
+  const tag = await Tag.findOneAndDelete({ _id: tagId });
+  if (!tag) {
+    throw new NotFoundError(`Tag with id ${tagId} does not exist`);
+  }
+  res.status(StatusCodes.OK).send();
 };
--- routes/tag.js
+++ routes/tag.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { createTag, getAllTags, getTag, updateTag, } from "../controllers/tag.js";
+import { createTag, getAllTags, getTag, updateTag, delete } from "../controllers/tag.js";
 
 const router = express.Router();
 
 router.route("/").post(createTag).get(getAllTags);
-router.route("/:tagId").get(getTag).put(updateTag)
+router.route("/:tagId").get(getTag).put(updateTag).delete()
 
 export default router;
