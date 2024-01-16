c428e8a7b478a12ea9fb4ad33e946ae3d2d32c5f
chryzcode <alabaolanrewaju13@gmail.com>
2024-01-16 06:14:10 +0100

    push

--- controllers/likes.js
+++ controllers/likes.js
@@ -4,11 +4,11 @@ import { StatusCodes } from "http-status-codes";
 export const like = async (req, res) => {
   const { postId } = req.params;
   const { userId } = req.user;
-  const liked = await Likes.find({ post: postId, user: userId });
+  const liked = await Likes.findOne({ post: postId, user: userId });
   if (liked) {
-    liked.deleteMany();
+    await Likes.findOneAndDelete({ post: postId, user: userId });
   }
-  const like = await Likes.create({ post: postId, user: userId });
+  await Likes.create({ post: postId, user: userId });
   const likes = (await Likes.find({ post: postId })).length;
   res.status(StatusCodes.OK).send(likes);
 };
