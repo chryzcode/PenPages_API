b8752b5997d17b835011e64a305f75468ee60b7b
chryzcode <alabaolanrewaju13@gmail.com>
2024-01-16 06:19:19 +0100

    push

--- controllers/likes.js
+++ controllers/likes.js
@@ -5,10 +5,12 @@ export const like = async (req, res) => {
   const { postId } = req.params;
   const { userId } = req.user;
   const liked = await Likes.findOne({ post: postId, user: userId });
+  console.log(liked);
   if (liked) {
     await Likes.findOneAndDelete({ post: postId, user: userId });
+  } else {
+    await Likes.create({ post: postId, user: userId });
   }
-  await Likes.create({ post: postId, user: userId });
   const likes = (await Likes.find({ post: postId })).length;
-  res.status(StatusCodes.OK).send(likes);
+  res.status(StatusCodes.OK).json({ likes: likes });
 };
