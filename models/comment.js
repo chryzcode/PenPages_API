01573d60750d8c5e08f3f6fe72164390e6fc1a3f
chryzcode <alabaolanrewaju13@gmail.com>
2024-01-17 07:57:20 +0100

    push

--- models/comment.js
+++ models/comment.js
@@ -52,5 +52,20 @@ const replyCommentSchema = new mongoose.Schema(
   }
 );
 
+
+const commentLikesSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: [true, "User field is required"],
+  },
+  post: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Comment",
+    required: [true, "User field is required"],
+  },
+});
+
 export const Comment = mongoose.model("Comment", commentSchema);
 export const replyComment = mongoose.model("replyComment", replyCommentSchema);
+export const commentLikes = mongoose.model("commentLikesSchema", commentLikes);
