c47432f2c4751d2048628737aca1bb26bcee3a46
chryzcode <alabaolanrewaju13@gmail.com>
2024-01-17 07:19:49 +0100

    push

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ import commentRouter from "./routes/comment.js";
 import postRouter from "./routes/post.js";
 import tagRouter from "./routes/tag.js";
 import authRouter from "./routes/user.js";
-import likesRouter from "./routes/likes.js";
+import likesRouter from "./routes/postLikes.js";
 
 // error handler
 import errorHandlerMiddleware from "./middleware/error-handler.js";
