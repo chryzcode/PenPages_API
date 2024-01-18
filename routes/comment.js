27bb49e4c91f58deaf4f992ab264942600739113
chryzcode <alabaolanrewaju13@gmail.com>
2024-01-18 04:52:39 +0100

    push

--- routes/comment.js
+++ routes/comment.js
@@ -9,6 +9,7 @@ import {
   deleteComment,
   deleteReplyComment,
   likePostComment,
+  likeReplyComment,
 } from "../controllers/comment.js";
 import authenticateUser from "../middleware/authentication.js";
 
@@ -17,7 +18,11 @@ const router = express.Router();
 router.route("/:postId").post(authenticateUser, createComment).get(getPostAllComments);
 router.route("/reply/:commentId").post(authenticateUser, createReplyComment).get(getPostAllReplyComments);
 router.route("/:commentId").put(authenticateUser, updateComment).delete(authenticateUser, deleteComment);
-router.route("/reply/:replyCommentId").put(authenticateUser, updateReplyComment).delete(authenticateUser, deleteReplyComment);
+router
+  .route("/reply/:replyCommentId")
+  .put(authenticateUser, updateReplyComment)
+  .delete(authenticateUser, deleteReplyComment);
 router.route("/like/:commentId").post(authenticateUser, likePostComment);
+router.route("/like/:replyCommentId").post(authenticateUser, likeReplyComment);
 
 export default router;
