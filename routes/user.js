435fe0ae70101672bf57d83903de40777d23cb46
chryzcode <alabaolanrewaju13@gmail.com>
2024-01-19 04:39:47 +0100

    push

--- routes/user.js
+++ routes/user.js
@@ -7,9 +7,9 @@ const router = express.Router();
 
 router.route("/auth/register").post(register);
 router.route("/auth/login").post(login);
-router.route("/auth/logout").post(authenticateUser, logout);
 router.route("/all-users").get(getAllUsers);
 router.route("/:username").get(getUser);
+router.route("/auth/logout").post(authenticateUser, logout);
 router.route("/update").put(authenticateUser, updateUser);
 router.route("/delete").put(authenticateUser, deleteUser);
 
