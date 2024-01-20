9e4ab4a6ec8978b53ba53d9abe26fa52a9a6012a
chryzcode <alabaolanrewaju13@gmail.com>
2024-01-20 07:19:27 +0100

    push

---
+++ utils/user.js
@@ -0,0 +1,12 @@
+import nodemailer from "nodemailer";
+import "dotenv/config";
+
+const transporter = nodemailer.createTransport({
+  port: 465, // true for 465, false for other ports
+  host: "smtp.gmail.com",
+  auth: {
+    user: process.env.Email_User,
+    pass: process.env.Email_Password,
+  },
+  secure: true,
+});
