c0002ea26fdb7797079b80a7db1a775e49905bd9
chryzcode <alabaolanrewaju13@gmail.com>
2024-01-22 19:01:56 +0100

    push

--- controllers/user.js
+++ controllers/user.js
@@ -7,7 +7,7 @@ import { transporter, generateToken } from "../utils/user.js";
 import { v4 as uuidv4 } from "uuid";
 
 const uniqueID = uuidv4();
-const doamin = process.env.DOMAIN || "http://127.0.0.1:8000/"
+const doamin = process.env.DOMAIN || "http://127.0.0.1:8000/";
 
 const linkVerificationtoken = generateToken(uniqueID);
 
@@ -75,8 +75,6 @@ export const logout = async (req, res) => {
   res.status(StatusCodes.OK).send();
 };
 
-
-
 export const forgotPassword = async (req, res) => {
   const { email } = req.body;
   if (!email) {
@@ -91,7 +89,7 @@ export const forgotPassword = async (req, res) => {
     to: user.email,
     subject: `${user.email} Forget your password`,
     text: "That was easy!",
-    html: `<p>Please use the following <a href="verify?token=${encodeURIComponent(
+    html: `<p>Please use the following <a href="${domain}verify?token=${encodeURIComponent(
       linkVerificationtoken
     )}">link</a> to verify your email. Link expires in 1 hour.</p>`,
   };
@@ -99,7 +97,7 @@ export const forgotPassword = async (req, res) => {
     if (error) {
       return console.log(error);
     }
-    res.status(StatusCodes.OK).send()
+    res.status(StatusCodes.OK).send();
   });
 };
 
