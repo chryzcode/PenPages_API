f6d61854809cc75a8703f0f00a0acb1fbd443978
chryzcode <alabaolanrewaju13@gmail.com>
2024-01-28 04:35:17 +0100

    push

--- app.js
+++ app.js
@@ -20,7 +20,11 @@ import notFoundMiddleware from "./middleware/not-found.js";
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(rateLimit());
+app.set('trust proxy', 1)
+app.use(rateLimit({
+  windowMs: 15 * 60 * 1000, //15 mins
+  max: 100, //limit each ip to 100 requests per windowsMs
+}));
 app.use(express.json());
 app.use(helmet());
 app.use(cors());
