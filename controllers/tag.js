32e5e07f2467ff08639bf06d0e4ffe0726798385
chryzcode <alabaolanrewaju13@gmail.com>
2024-02-12 06:26:50 +0100

    push

--- controllers/tag.js
+++ controllers/tag.js
@@ -6,9 +6,9 @@ import User from "../models/user.js";
 export const createTag = async (req, res) => {
   const userId = req.user.userId;
   const user = User.findOne({ _id: userId, admin: true });
-  // if (!user) {
-  //   throw new NotFoundError(`User with id ${userId} who is an admin does not exist`);
-  // }
+  if (!user) {
+    throw new NotFoundError(`User with id ${userId} who is an admin does not exist`);
+  }
   const tag = await Tag.create({ ...req.body });
   res.status(StatusCodes.CREATED).json({ tag });
 };
