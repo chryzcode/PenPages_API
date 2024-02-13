96913070d87785993528cde47961a74c3bd573c0
chryzcode <alabaolanrewaju13@gmail.com>
2024-02-13 21:30:33 +0100

    push

--- controllers/follower.js
+++ controllers/follower.js
@@ -1,6 +1,7 @@
-import follower from "../models/follower";
+import Follower from "../models/follower";
 import User from "../models/user";
 import { BadRequestError, UnauthenticatedError, NotFoundError } from "../errors/index.js";
+import { StatusCodes } from "http-status-codes";
 
 export const followUnfollowUser = async (req, res) => {
   const userId = req.user.userId;
@@ -13,4 +14,11 @@ export const followUnfollowUser = async (req, res) => {
   if (!author) {
     throw new NotFoundError(`author/ account with ${authorId} does not exist`);
   }
+  if (user == author) {
+    throw new BadRequestError("You can not follow/ unfolloe yourself");
+  }
+  req.body.user = authorId;
+  req.body.follower = userId;
+  const follower = await Follower.create({ ...req.body });
+  res.status(StatusCodes.OK).json({ follower });
 };
