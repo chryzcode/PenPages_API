eb1f62bc71cecaab77d1def86b1b258f04c95086
chryzcode <alabaolanrewaju13@gmail.com>
2024-02-13 03:35:04 +0100

    push

---
+++ models/notification.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const notificationSchema = new mongoose.Schema(
+  {
+    fromUser: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: [true, "The from user field is required"],
+    },
+    toUser: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: [true, "The to user field is required"],
+    },
+    info: {
+      type: String,
+      required: [true, "The info field is required"],
+    },
+    url: {
+      type: String,
+      required: [true, "The url field is required"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("Notification", notificationSchema);
