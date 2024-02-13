0694a0bebbbd29638110dc7d1495a8151850e3b1
chryzcode <alabaolanrewaju13@gmail.com>
2024-02-13 21:44:44 +0100

    push

--- controllers/follower.js
+++ controllers/follower.js
@@ -4,21 +4,28 @@ import { BadRequestError, UnauthenticatedError, NotFoundError } from "../errors/
 import { StatusCodes } from "http-status-codes";
 
 export const followUnfollowUser = async (req, res) => {
-  const userId = req.user.userId;
-  const { authorId } = req.params;
+  const followerId = req.user.userId;
+  const { userId } = req.params;
+  const follower = await User.findOne({ _id: followerId });
+  if (!follower) {
+    throw new NotFoundError(`user/ follower with ${followerId} does not exist`);
+  }
   const user = await User.findOne({ _id: userId });
   if (!user) {
-    throw new NotFoundError(`user with ${userId} does not exist`);
+    throw new NotFoundError(`user/ author with ${userId} does not exist`);
   }
-  const author = await User.findOne({ _id: authorId });
-  if (!author) {
-    throw new NotFoundError(`author/ account with ${authorId} does not exist`);
+  if (follower == user) {
+    throw new BadRequestError("You can not follow/ unfollow yourself");
   }
-  if (user == author) {
-    throw new BadRequestError("You can not follow/ unfolloe yourself");
+  req.body.user = userId;
+  req.body.follower = followerId;
+  const followerObj = await Follower.findOne({ user: userId, follower: followerId });
+  if (followerObj) {
+    await Follower.deleteOne({ user: userId, follower: followerId });
+  } else {
+    await Follower.create({ ...req.body });
   }
-  req.body.user = authorId;
-  req.body.follower = userId;
-  const follower = await Follower.create({ ...req.body });
-  res.status(StatusCodes.OK).json({ follower });
+  const followersCount = (await Follower.find({ user: userId })).length;
+  console.log(followersCount);
+  res.status(StatusCodes.OK).send();
 };
