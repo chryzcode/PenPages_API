910c9f567a5f257ceb9f84bcca7704d2a3a76538
chryzcode <alabaolanrewaju13@gmail.com>
2024-02-19 03:30:45 +0100

    push

---
+++ routes/notification.js
@@ -0,0 +1,13 @@
+import express from "express";
+import {
+  allNotifications,
+  markNotificationRead,
+  allReadNotifications,
+  allUnreadNotifications,
+  markUnreadNotificationsRead,
+} from "../controllers/notification.js";
+import authenticateUser from "../middleware/authentication.js";
+
+const router = express.Router;
+
+router("").get;
