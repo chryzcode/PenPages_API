94446535592ec11d2b783ebe4d212defc5755663
chryzcode <alabaolanrewaju13@gmail.com>
2024-05-12 12:31:52 +0100

    push

--- routes/follower.js
+++ routes/follower.js
@@ -9,6 +9,6 @@ const router = express.Router();
 router.route("/follow-unfollow/:userId").post(authenticateUser, followUnfollowUser);
 router.route("/count/:userId").get(userFollowersCount);
 router.route("/:userId/followers").get(userFollowers);
-router.route("/:userId/following/count").get(userFollowingCount);
+router.route("/:username/following/count").get(userFollowingCount);
 
 export default router;
