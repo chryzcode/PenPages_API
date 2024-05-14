b2ae08834250d588f616dba6e86f52373b6769e0
chryzcode <alabaolanrewaju13@gmail.com>
2024-05-14 03:43:04 +0100

    push

--- routes/user.js
+++ routes/user.js
@@ -28,7 +28,7 @@ router.route("/update").put(authenticateUser, updateUser);
 router.route("/update/password").put(authenticateUser, updatePassword);
 router.route("/delete").delete(authenticateUser, deleteUser);
 router.route("/send-forgot-password-link").post(sendForgotPasswordLink);
-router.route("/auth/forgot-password/:userId/:token").post(verifyForgotPasswordToken);
-router.route("/auth/verify-account/:userId/:token").post(verifyAccount);
+router.route("/auth/forgot-password/:userId/:token").get(verifyForgotPasswordToken);
+router.route("/auth/verify-account/:userId/:token").get(verifyAccount);
 
 export default router;
